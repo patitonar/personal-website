f1f74d3c17ef7e14ccca94dd47af6746db357791
Gerardo Nardelli <patitonardelli@gmail.com>
2017-02-21 19:38:27 -0300

    Inline Body styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './assets/font-awesome/css/font-awesome.min.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const MuiWrapper = () => (
