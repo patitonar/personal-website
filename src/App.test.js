55ada5f600cd2c2ea646c141f4c2f31ddc9a5cae
Gerardo Nardelli <patitonardelli@gmail.com>
2017-07-09 17:58:39 -0300

    Remove material-ui

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { StyleSheetTestUtils } from 'aphrodite';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const MuiWrapper = () => (
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>
-);
-
-
-describe('Test suite for App', () => {
-  beforeEach(() => {
-      StyleSheetTestUtils.suppressStyleInjection();
-  });
-
-  afterEach(() => {
-      StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
-  it('Full App renders correctly', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<MuiWrapper />, div);
-  });
+it('Full App renders correctly', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-const MuiWrapper = () => (
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>
-);
 
 ReactDOM.render(
-  <MuiWrapper />,
+  <App />,
   document.getElementById('root')
 );
